0cce708c9b61599ab46220979b6c5e7fc05ab222
EmeraldNM <zlovelady01@gmail.com>
2023-03-19 20:37:04 -0500

    Fixing up some things in add_func()

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -70,8 +70,8 @@ async def add_func(Server_id, Function_name, arguments):
     @guild_only()
     async def temp(ctx):
         interaction = await ctx.respond("Handing command to server!")
-        origin = await interaction.original_message()
-        run((ctx.command.name), origin.channel.name)
+        origin = await interaction.original_response()
+        run((ctx.command.name), origin.guild_id, origin.guild.name, origin.channel_id, origin.channel.name)
 
     await bot.sync_commands(force = True, guild_ids=[Server_id])
 
