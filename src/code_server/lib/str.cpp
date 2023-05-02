2026fdeb723f9618a7ae9ef326acdc6be8df7866
peytonblake <psbkf8@mst.edu>
2023-05-01 22:45:17 -0500

    Added several stringlib functions
    Strip, Upper, Lower, PopBack, Count, FirstOf

--- src/code_server/lib/str.cpp
+++ src/code_server/lib/str.cpp
@@ -24,3 +24,55 @@ std::shared_ptr<discode::Data> lib_str::Substr::execute(discode::VM * vm, std::v
     return std::make_shared<discode::String>(str.substr(left_idx, len));
 }
 
+std::shared_ptr<discode::Data> lib_str::Strip::execute(discode::VM * vm, std::vector<std::shared_ptr<discode::Data>> data) {
+    auto orig = data.at(0)->getString();
+    auto delims = data.at(1)->getString();
+    auto result = util::strip(orig, delims);
+    return std::make_shared<discode::String>(result);
+}
+
+std::shared_ptr<discode::Data> lib_str::Upper::execute(discode::VM * vm, std::vector<std::shared_ptr<discode::Data>> data) {
+    auto orig = data.at(0)->getString();
+    for (size_t i = 0; i < orig.length(); i++) { orig[i] = std::toupper(orig[i]); }
+    return std::make_shared<discode::String>(orig);
+}
+
+std::shared_ptr<discode::Data> lib_str::Lower::execute(discode::VM * vm, std::vector<std::shared_ptr<discode::Data>> data) {
+    auto orig = data.at(0)->getString();
+    for (size_t i = 0; i < orig.length(); i++) { orig[i] = std::tolower(orig[i]); }
+    return std::make_shared<discode::String>(orig);
+}
+
+std::shared_ptr<discode::Data> lib_str::PopBack::execute(discode::VM * vm, std::vector<std::shared_ptr<discode::Data>> data) {
+    auto orig = data.at(0)->getString();
+    if (orig.empty()) { return std::make_shared<discode::Null>(); }
+    orig.pop_back();
+    return std::make_shared<discode::String>(orig);
+}
+
+std::shared_ptr<discode::Data> lib_str::Count::execute(discode::VM * vm, std::vector<std::shared_ptr<discode::Data>> data) {
+    auto orig = data.at(0)->getString();
+    auto key = data.at(1)->getString();
+    int counter = 0;
+    for (size_t i = 0; i < orig.length(); i++) {
+        if (orig[i] == key[0]) { counter++; }
+    }
+    return std::make_shared<discode::Number>(counter);
+}
+
+std::shared_ptr<discode::Data> lib_str::FirstOf::execute(discode::VM * vm, std::vector<std::shared_ptr<discode::Data>> data) {
+    auto orig = data.at(0)->getString();
+    auto key = data.at(1)->getString();
+    auto keyLen = key.length();
+    for (size_t i = 0; i < orig.length(); i++) {
+        if (orig[i] == key[0] && i + keyLen <= orig.length()) {
+            size_t inc = 0;
+            while (i + inc < orig.length() && orig[i+inc] == key[inc]) { 
+                inc++;
+                if (inc == keyLen) { return std::make_shared<discode::Number>(i); }
+            }
+        }
+    }
+    auto result = key + " not found";
+    return std::make_shared<discode::String>(result);
+}
