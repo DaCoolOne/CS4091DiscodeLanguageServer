2af9c1a2977369e6d314549118afd10a7df172d4
unknown <bchkzb@umsystem.edu>
2023-05-02 18:19:09 -0500

    fixed a small issue

--- src/code_server/lib/role.cpp
+++ src/code_server/lib/role.cpp
@@ -8,7 +8,7 @@
 std::shared_ptr<discode::Data> lib_role::Send::execute(discode::VM * vm, std::vector<std::shared_ptr<discode::Data>> data) {
     
     json::JsonObject obj;
-    obj.add("Name", std::make_shared<json::JsonString>("interaction"));
+    obj.add("Name", std::make_shared<json::JsonString>("role"));
     obj.add("Server_ID", std::make_shared<json::JsonString>(data.at(0)->getString()));
     obj.add("Role", std::make_shared<json::JsonString>(data.at(1)->getString()));
     obj.add("User", std::make_shared<json::JsonString>(data.at(2)->getString()));
